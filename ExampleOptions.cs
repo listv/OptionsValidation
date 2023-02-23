8afa7237966bbb0fdc27e2e097bf407019277cce
Alex <okuzia@softserveinc.com>
2023-02-23 16:57:43 +0200

    Add options section

---
+++ ExampleOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace OptionsValidation;
+
+public class ExampleOptions
+{
+    public const string SectionName = "Example";
+
+    public string LogLevel { get; init; }
+    public int Retries { get; init; }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.Extensions.Options;
+using OptionsValidation;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOptions<ExampleOptions>()
+    .Bind(builder.Configuration.GetSection(ExampleOptions.SectionName));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/", (IOptions<ExampleOptions> options) => options.Value);
 
 app.Run();
