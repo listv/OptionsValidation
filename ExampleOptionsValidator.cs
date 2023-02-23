7b98f81ca06aed4f1a46df4c795d8f4f1480621c
Alex <okuzia@softserveinc.com>
2023-02-23 17:26:12 +0200

    Add options fluent validation

---
+++ ExampleOptionsValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace OptionsValidation;
+
+public class ExampleOptionsValidator : AbstractValidator<ExampleOptions>
+{
+    public ExampleOptionsValidator()
+    {
+        RuleFor(options => options.LogLevel)
+            .IsEnumName(typeof(LogLevel));
+
+        RuleFor(options => options.Retries)
+            .InclusiveBetween(1, 9);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
+using FluentValidation;
 using Microsoft.Extensions.Options;
 using OptionsValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<IValidator<ExampleOptions>, ExampleOptionsValidator>();
+
 builder.Services.AddOptions<ExampleOptions>()
-    .Bind(builder.Configuration.GetSection(ExampleOptions.SectionName));
+    .Bind(builder.Configuration.GetSection(ExampleOptions.SectionName))
+    .ValidateFluently()
+    .ValidateOnStart();
 
 var app = builder.Build();
 
